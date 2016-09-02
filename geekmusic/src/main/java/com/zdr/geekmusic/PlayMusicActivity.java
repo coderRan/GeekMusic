bb92f14c4e6d6dcf6d16a32cbfa086cb132b406c
= <coder.0012ff7c@gmail.com>
2016-09-02 22:54:04 +0800

    添加中间的MusicView转动界面，并实现seekBar的拖动联动

--- geekmusic/src/main/java/com/zdr/geekmusic/PlayMusicActivity.java
+++ geekmusic/src/main/java/com/zdr/geekmusic/PlayMusicActivity.java
@@ -1,118 +1,149 @@
 package com.zdr.geekmusic;
 
-
+import android.graphics.Color;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.zdr.geekmusic.entity.Music;
+import com.zdr.geekmusic.utils.Method;
 
 import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import co.mobiwise.playerview.MusicPlayerView;
 
 public class PlayMusicActivity extends AppCompatActivity {
 
-    @BindView(R.id.btn_start)
-    Button btnStart;
-    @BindView(R.id.btn_stop)
-    Button btnStop;
-    @BindView(R.id.btn_pause)
-    Button btnPause;
-    @BindView(R.id.sb_progress)
-    SeekBar sbProgress;
+
+    @BindView(R.id.mpv)
+    MusicPlayerView mpv;
+    @BindView(R.id.tv_music_name)
+    TextView tvMusicName;
+    @BindView(R.id.tv_music_artist)
+    TextView tvMusicArtist;
+    @BindView(R.id.sb_progress_bar)
+    SeekBar sbProgressBar;
+    @BindView(R.id.tv_currTime)
+    TextView tvCurrTime;
+    @BindView(R.id.tv_Duration)
+    TextView tvDuration;
 
     private Music music;
     private MediaPlayer player;
+    private Handler handler = new Handler();
 
-    private Handler playHandle = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            sbProgress.setProgress(msg.what);
+    private void init() {
+        music = (Music) (getIntent().getExtras().get("music"));
+        mpv.setCoverDrawable(R.mipmap.mycover);
+        mpv.setProgressVisibility(false);
+        tvMusicName.setText(music.getName());
+        tvMusicArtist.setText(music.getArtist());
+        player = MediaPlayer.create(this, Uri.parse(music.getPath()));
+        //设置seekBar
+        sbProgressBar.setDrawingCacheBackgroundColor(Color.argb(100,100,100,100));
+        sbProgressBar.setMax(player.getDuration());
+        tvDuration.setText(Method.secondsToTime(player.getDuration()/1000));
+        tvCurrTime.setText(Method.secondsToTime(0));
+        //加载音乐
+        player.reset();
+        try {
+            player.setDataSource(music.getPath());
+            player.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    };
-    private Thread seekbarThread;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_music);
         ButterKnife.bind(this);
-        music = (Music) (getIntent().getExtras().get("music"));
-        player = new MediaPlayer();
+        init();
 
-        sbProgress.setMax(Integer.parseInt(music.getDurationMs()));
-        seekbarThread = new Thread(new Runnable() {
+        //中间点击暂停的按钮监听
+        mpv.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                long time = Long.parseLong(music.getDurationMs());
-                int  currTime = 0;
-                try {
-                    while (currTime<time){
-                        Thread.sleep(100);
-                        currTime += 100;
-                        Log.e("currTime", currTime + "");
-                        playHandle.sendEmptyMessage(currTime);
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            public void onClick(View v) {
+                if (mpv.isRotating()) {
+                    mpv.stop();
+                    player.pause();
+                } else {
+                    mpv.start();
+                    handler.post(start);
                 }
             }
         });
 
-        sbProgress.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        sbProgressBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
+                tvCurrTime.setText(Method.secondsToTime(progress/1000));
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
+                player.pause();
+                if (mpv.isRotating())
+                    mpv.stop();
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
+                player.seekTo(sbProgressBar.getProgress());
+                player.start();
 
+                if (!mpv.isRotating())
+                    mpv.start();
             }
         });
-
     }
 
-    @OnClick({R.id.btn_start, R.id.btn_stop, R.id.btn_pause})
+    @OnClick({R.id.tv_music_artist, R.id.sb_progress_bar})
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.btn_start:
-                try {
-                    player.reset();
-                    player.setDataSource(music.getPath());
-                    player.prepare();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                seekbarThread.start();
-                player.start();
+            case R.id.tv_music_artist:
                 break;
-            case R.id.btn_stop:
-                if (player.isPlaying())
-                    player.stop();
-                break;
-            case R.id.btn_pause:
-                player.pause();
-                try {
-                    seekbarThread.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            case R.id.sb_progress_bar:
                 break;
         }
     }
 
+    Runnable start = new Runnable() {
+        @Override
+        public void run() {
+            player.start();
+            handler.post(updatesb);
+            //用一个handler更新SeekBar
+        }
+
+    };
+    Runnable updatesb = new Runnable() {
+        @Override
+        public void run() {
+            sbProgressBar.setProgress(player.getCurrentPosition());
+            handler.postDelayed(updatesb, 1000);
+            //每秒钟更新一次
+        }
+
+    };
+
+    @Override
+    public void finish() {
+        player.release();
+        super.finish();
+
+    }
 }
+/**
+ * 1. 打开播放界面后，直接滑动进度条，不播放音乐。
+ * 2. 在播放音乐时点击进度条，中间的图标不变
+ * */
---
+++ geekmusic/src/main/java/com/zdr/geekmusic/utils/Method.java
@@ -0,0 +1,26 @@
+package com.zdr.geekmusic.utils;
+
+/**
+ * 方法类
+ * Created by zdr on 16-9-2.
+ */
+public class Method {
+
+    /**
+     * 返回一个时间格式为04：35
+     * @param seconds 毫秒值
+     * @return 格式化后的时间
+     */
+    public static String secondsToTime(int seconds) {
+        String time;
+        String minutesText = String.valueOf(seconds / 60);
+        if (minutesText.length() == 1) minutesText = "0" + minutesText;
+
+        String secondsText = String.valueOf(seconds % 60);
+        if (secondsText.length() == 1) secondsText = "0" + secondsText;
+
+        time = minutesText + ":" + secondsText;
+
+        return time;
+    }
+}
